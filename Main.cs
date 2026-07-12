5d8b71ad65dfa1478aa245640d4888ff6b13c0a8
Logan <logankeenan55@gmail.com>
2026-07-12 19:37:50 -0400

    made working Crytek SSAO shader

--- Main.cs
+++ Main.cs
@@ -1,16 +1,23 @@
+using System;
 using Godot;
 
 public partial class Main : Node3D
 {
 
     public override void _Ready(){
+		createRandomPoints(20,2.0f,1.0f);
 	}
 
     public override void _Process(double delta){
 		if (Input.IsActionJustPressed("esc")){
 			GetTree().Quit(); 
+			
 		}
     }
 
-
+	public void createRandomPoints(int count,float max, float min){
+		for(int i = 0; i < count; i++){
+			GD.Print("vec3(",Math.Round(GD.Randf()*max-min,2),",",Math.Round(GD.Randf()*max-min,2),",",Math.Round(GD.Randf()*max-min,2),"),");
+		}
+	}
 }
