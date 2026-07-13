888206027b7e7868157fb8dba9ec40e84f680ecc
Logan <logankeenan55@gmail.com>
2026-07-13 18:13:06 -0400

    Added random noise rotation and vent to scene

--- Main.cs
+++ Main.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using Godot;
 
 public partial class Main : Node3D
 {
 	private ShaderMaterial material;
 	private MeshInstance3D shaderOverlay;
+
+	Image ssaoNoiseImage = Image.CreateEmpty(4, 4, false, Image.Format.Rgb8);
+	ImageTexture ssaoNoiseTexture;
 	[Export]
 	private int mode;
 
@@ -12,6 +17,10 @@ public partial class Main : Node3D
     public override void _Ready(){
 		shaderOverlay = GetNode<Camera3D>("Camera").GetNode<MeshInstance3D>("SSAO");
 		material = (ShaderMaterial)shaderOverlay.GetActiveMaterial(0);
+		
+		setssaoNoiseTexture();
+		material.SetShaderParameter("noiseTexture",ssaoNoiseTexture);
+
 		material.SetShaderParameter("mode",mode);
 		//createRandomPoints(200,2.0f,1.0f);
 
@@ -29,5 +38,16 @@ public partial class Main : Node3D
 		for(int i = 0; i < count; i++){
 			GD.Print("vec3(",Math.Round(GD.Randf()*max-min,2),",",Math.Round(GD.Randf()*max-min,2),",",Math.Round(GD.Randf()*max-min,2),"),");
 		}
+	}
+
+	public void setssaoNoiseTexture(){
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 4; j++){
+				Vector3 value = new Vector3(GD.Randf(),GD.Randf(),0).Normalized();
+
+				ssaoNoiseImage.SetPixel(i, j, new Color(value.X,value.Y,0.5f));
+			}
+		}
+				ssaoNoiseTexture = ImageTexture.CreateFromImage(ssaoNoiseImage);
 	}
 }
