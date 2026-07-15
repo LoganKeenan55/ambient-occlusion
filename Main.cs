0587e3e2f4d7653232a289ae903f32edf955a253
Logan <logankeenan55@gmail.com>
2026-07-14 23:08:28 -0400

    test

--- Main.cs
+++ Main.cs
@@ -7,10 +7,13 @@ public partial class Main : Node3D
 {
 	private ShaderMaterial material;
 	private MeshInstance3D shaderOverlay;
+<<<<<<< HEAD
 	private MeshInstance3D blurOverlay;
 	private Camera3D ssaoCamera;
 
 	private Camera3D blurCamera;
+=======
+>>>>>>> parent of ef9ec30 (nothing works everything is broken)
 
 	Image ssaoNoiseImage = Image.CreateEmpty(4, 4, false, Image.Format.Rgb8);
 	ImageTexture ssaoNoiseTexture;
@@ -19,16 +22,23 @@ public partial class Main : Node3D
 
 
     public override void _Ready(){
+<<<<<<< HEAD
 		ssaoCamera = GetNode<Camera3D>("SSAOCamera");
 
 		shaderOverlay = GetNode<MeshInstance3D>("SSAOCamera/SSAO");
+=======
+		shaderOverlay = GetNode<Camera3D>("Camera").GetNode<MeshInstance3D>("SSAO");
+>>>>>>> parent of ef9ec30 (nothing works everything is broken)
 		material = (ShaderMaterial)shaderOverlay.GetActiveMaterial(0);
 		
 		setssaoNoiseTexture();
 		material.SetShaderParameter("noiseTexture",ssaoNoiseTexture);
+<<<<<<< HEAD
 		//blurOverlay = GetNode<MeshInstance3D>("SubViewportContainer/BlurViewport/BlurCamera/Blur");
 		//ShaderMaterial blurMaterial = (ShaderMaterial)blurOverlay.GetActiveMaterial(0);
 		
+=======
+>>>>>>> parent of ef9ec30 (nothing works everything is broken)
 
 		material.SetShaderParameter("mode",mode);
 		//createRandomPoints(200,2.0f,1.0f);
@@ -37,7 +47,10 @@ public partial class Main : Node3D
 	}
 
     public override void _Process(double delta){
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of ef9ec30 (nothing works everything is broken)
 		if (Input.IsActionJustPressed("esc")){
 			GetTree().Quit(); 
 			
